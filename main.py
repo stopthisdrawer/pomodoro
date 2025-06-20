1d4805fc00ac6c70d304ddbd89b848432c904c65
stopthisdrawer <itegrateddrawer@gmial.com>
2025-06-20 09:46:38 +0300

    created methods for working with settings

--- main.py
+++ main.py
@@ -1,52 +1,58 @@
 import time
+import json
 
-work_time = 1
-break_time = 1
-long_break_time = 1
-
-def start_timer(wt, bt, lbt):
-    while wt != 0:
-        print(f'{wt//60}:{wt%60}')
-        wt-=1
-        time.sleep(1)
-    print('Time to break')
-    while bt != 0:
-        print(f'{bt//60}:{bt%60}')
-        bt-=1
-        time.sleep(1)
-    print('Time to work!')
+class Timer():
+    def __init__(self):
+        self.load_settings()
+        self.current_state = 'WORK'
 
 
-def settings():
-    pass
+    def save_settings(self, dict):
+        with open('settings.json', 'w') as f:
+            json.dump(dict, f)
 
 
-def stats():
-    pass
+    def load_settings(self):
+        try:
+            with open ('settings.json', 'r') as f:
+                settings = f.read()
+        except:
+            self.settings = {
+                'work_time': 25,
+                'break_time': 5,
+                'long_break_time': 15,
+                'auto_work_starting': False,
+                'auto_break_starting': True
+            }
+            self.save_settings(self.settings)
 
 
-def control():
-    flag =True
-    while flag:
-        command = input('Type a command: ')
-        match command:
-            case 'start':
-                start_timer(work_time, break_time, )
-            case 'settings':
-                settings()
-            case 'stats':
-                stats()
-            case 'exit': exit()
-            case _:
-                print("""
+    def control(self):
+        flag =True
+        while flag:
+            command = input('Type a command: ')
+            match command:
+                case 'start':
+                    self.start_timer(self.work_time, self.break_time, self.long_brre)
+                case 'settings':
+                    self.update_settings()
+                case 'change':
+                    self.change_state()
+                case 'stats':
+                    self.check_stats()
+                case 'exit': exit()
+                case _:
+                    print("""
 Unknown command. Use this:
--start: start timer
--stats: statistics of using pomodoro timer
--settings: change settings
--exit: exit from timer
-""")
+    -start: start timer
+    -change: change state of timer
+    -stats: statistics of using pomodoro timer
+    -settings: change settings
+    -exit: exit from timer
+    """)
         
 
 
 if __name__ == '__main__':
-    control()
+    timer = Timer()
+    timer.control()
